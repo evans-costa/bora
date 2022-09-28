b1248b3b2a6649496c1c778519060ed7d317f0da
jose <josecarlosms7f@gmail.com>
2022-09-27 21:09:32 -0300

    tela de cadastro e menu bar atualizado

---
+++ tela-cadastro/cadastro.js
@@ -0,0 +1,45 @@
+let formCadastro = document.getElementById("form-cadastro")
+
+const error = []
+let inputPrimeiroNome = document.getElementById("primeironome");
+let labelPrimeiroNome = document.getElementById("labelPrimeiroNome");
+
+let inputSobrenome = document.querySelector("#sobrenome");
+let labelSobrenome = document.getElementById("labelSobrenome");
+
+let inputTelefone = document.getElementById("telefone");
+let labelTelefone = document.getElementById("labelTelefone");
+
+let inputEmail = document.getElementById("email");
+let labelEmail = document.getElementById("labelEmail");
+
+
+let inputcpf = document.getElementById("cpf");
+let labelcpf = document.getElementById("labelcpf");
+
+
+let inputAniversario = document.getElementById("aniversario");
+let labelAniversario = document.getElementById("labelAniversario");
+
+let inputRua = document.getElementById("rua");
+let labelRua = document.getElementById("labelRua");
+
+let inputNumero = document.getElementById("numero");
+let labelNumero = document.getElementById("labelNumero");
+
+let inputCep = document.getElementById("cep");
+let labelCep = document.getElementById("labelCep");
+
+let inputCidade = document.getElementById("cidade");
+let labelCidade = document.getElementById("labelCidade");
+
+let inputEstado = document.getElementById("estado");
+let labelEstado = document.getElementById("labelEstado");
+
+let inputSenha = document.getElementById("senha");
+let labelSenha = document.getElementById("labelSenha");
+
+let inputConfSenha = document.getElementById("confirmaSenha");
+let labelConfSenha = document.getElementById("labelConfSenha");
+
+

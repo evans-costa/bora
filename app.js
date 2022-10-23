31e5034a46d0f9d3acd6cc986e9bb4dbfe903f27
Jcmendes7 <josecarlosms7f@gmail.com>
2022-10-22 22:30:01 -0300

    styles tela cadastro

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 
 
 const indexRouter = require('./routes/rotaIndex');
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.use("/", indexRouter);
 app.use("/eventos", eventosRouter);

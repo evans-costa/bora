be4db9ff56a69aec3913e334ac30fa4af91c3ba3
danielcfleite <danielcfleite@gmail.com>
2022-10-26 20:51:00 -0300

    fetch-cep-fix

--- routes/rotaEventos.js
+++ routes/rotaEventos.js
@@ -1,19 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const EventosController = require("../controllers/EventosController")
-const multerUpload = require('../config/multer')
+const EventosController = require("../controllers/EventosController");
+const multerUpload = require("../config/multer");
 
 // Ir para a tela eventos
-router.get('/', EventosController.telaEventos)
+router.get("/", EventosController.telaEventos);
 
 // Ir para a tela de cadastro e criar um novo Evento
-router.get('/cadastroevento', EventosController.telaCadastroEvento)
-router.post('/cadastroevento', multerUpload.single('file'), EventosController.criarEvento)
-
+router.get("/cadastroevento", EventosController.telaCadastroEvento);
+router.post(
+  "/cadastroevento",
+  multerUpload.single("file"),
+  EventosController.criarEvento
+);
 
 // Ir para a tela de edição e alterar ou excluir um Evento
-router.get('/:id/editarevento', EventosController.telaEditarEvento)
-router.put('/:id/editarevento', multerUpload.single('file'), EventosController.atualizarPorId)
-router.delete('/:id/editarevento', EventosController.excluirPorId)
+router.get("/:id/editarevento", EventosController.telaEditarEvento);
+router.put(
+  "/:id/editarevento",
+  multerUpload.single("file"),
+  EventosController.atualizarPorId
+);
+router.delete("/:id/editarevento", EventosController.excluirPorId);
 
 module.exports = router;

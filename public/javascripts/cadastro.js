9c364c5d04a700a04c7ffdade82d3dc7062c04f3
Evandro <evandro.souzac@gmail.com>
2023-01-19 13:17:02 -0300

    Novo modal no menu hamburguer

--- public/javascripts/cadastro.js
+++ public/javascripts/cadastro.js
@@ -1,13 +1,3 @@
-const menuModal = document.getElementById("menu-modal");
-const navIcon2 = document.querySelector("#nav-icon2");
-
-navIcon2.addEventListener("click", () => {
-  if (navIcon2.classList.toggle("open")) {
-    menuModal.style.display = "block";
-  } else {
-    menuModal.style.display = "none";
-  }
-});
 // variaveis dos inputs e labels
 
 const formCadastro = document.getElementById("form-cadastro");

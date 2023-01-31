336a1b2462225172a1bf133a1f3d7688f849fd3f
Jcmendes7 <josecarlosms7f@gmail.com>
2023-01-31 13:17:09 -0300

    banco de dados fale conosco criado, models/Contact.js

--- routes/rotaFaleConosco.js
+++ routes/rotaFaleConosco.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const faleConosco = require('../controllers/FaleConoscoController');
 
 router.get('/', faleConosco.entrarEmContato);
+router.post('/', faleConosco.faleConoscoCreate);
 
 module.exports = router;

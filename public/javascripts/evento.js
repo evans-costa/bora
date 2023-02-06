2540cb8dc284bc9ae2d3f6457a01dda052da9005
danielcfleite <danielcfleite@gmail.com>
2023-02-06 19:04:20 -0300

    minor-fix-toast

--- public/javascripts/evento.js
+++ public/javascripts/evento.js
@@ -1,17 +1,23 @@
 const toastTrigger = document.getElementById('comprarEvento');
 const toastLiveExample = document.getElementById('liveToast');
+const btContent = document.getElementById('comprarEvento')
 
 function barra() {
   let barWidth = 100;
   const progressBar = document.getElementById("barra");
   const interval = setInterval(diminuirBarra, 50);
+  btContent.innerHTML = ' <i class="bi bi-cart"></i> Item já adicionado ao carrinho'
+  btContent.classList.add('disabled')
 
   function diminuirBarra() {
     if (barWidth == 0) {
       clearInterval(interval);
 
+
       setTimeout(() => {
         progressBar.style.width = "100%";
+        btContent.classList.remove('disabled')
+        btContent.innerHTML = ' <i class="bi bi-cart"></i> COMPRAR'
       }, 1000);
 
     } else {
